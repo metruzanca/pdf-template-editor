b2e36d01e687ddde46c6e8644f88bef088049e6b
Samuele Zanca <samuele.zanca@mia-platform.eu>
2020-10-08 14:28:19 +0200

    intial

---
+++ src/components/Editor/HandlebarsProvider.ts
@@ -0,0 +1,25 @@
+// import { languages, editor, Position } from 'monaco-editor';
+// import { Monaco } from '@monaco-editor/react';
+
+// const provider = instance.languages.registerCompletionItemProvider(
+//   'handlebars',
+//   {
+//     triggerCharacters: ['{', '.'],
+//     provideCompletionItems: (model, position) => {
+//       const suggestions = buildSuggestions(
+//         instance,
+//         propSuggestions,
+//         model,
+//         position
+//       );
+
+//       return {
+//         suggestions
+//       } as languages.ProviderResult<languages.CompletionList>;
+//     }
+//   }
+// );
+
+// disposables.current.push(provider);
+
+// https://github.com/microsoft/monaco-editor/issues/1731
